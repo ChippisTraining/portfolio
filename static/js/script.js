7d756889b82d64ce5f6fc61eba5bd3a2942cb7f2
Dominik Krenn <dominik.krenn@1tool.com>
2025-02-17 11:53:52 +0100

    Remove unused JavaScript for navigation button functionality

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll('.navigation-option');
-console.log(buttons);
-for (const button of buttons) {
-    button.addEventListener('click', () => {
-        window.location.href = button.getAttribute('href') || '/';
-    });
-}
